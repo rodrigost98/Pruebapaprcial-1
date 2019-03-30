af2727bd4011bc016da6c566cb034c585fd7f561
liderz250@gmail.com <liderz250@gmail.com>
2019-03-30 12:58:46 -0400

    Commit con modelo web API

---
+++ parcial/API Parcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace API_Parcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<API_Parcial.Models.Product> Products { get; set; }
+    }
+}
