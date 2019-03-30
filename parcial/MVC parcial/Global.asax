f0b68f5104f5b55dce7f20bc3291f3d001268db7
liderz250@gmail.com <liderz250@gmail.com>
2019-03-30 12:05:53 -0400

    Commit  con proyecto mvc creado

---
+++ parcial/MVC parcial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_parcial.MvcApplication" Language="C#" %>
---
+++ parcial/MVC parcial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_parcial.Startup))]
+namespace MVC_parcial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
